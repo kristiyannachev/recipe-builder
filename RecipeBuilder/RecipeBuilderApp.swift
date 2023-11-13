561e7bb790e2c7d7b6afe95d9b0f3bb80289d237
ga27kig <ga27kig@mytum.de>
2023-11-13 14:29:17 +0100

    Initial Commit

---
+++ RecipeBuilder/RecipeBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBuilderApp.swift
+//  RecipeBuilder
+//
+//  Created by Kristiyan Nachev on 13.11.23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
